ed9fcf20ef29bc3ebf0e8b24044e910811df7423
ALDO <aldo@aldos-mbp.attlocal.net>
2021-02-15 18:45:35 -0800

    first

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,11 +1,16 @@
 // You do not need to change any code in this file for MVP
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faHeart, faCircle, faCompass } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import {
+  faHeart,
+  faCircle,
+  faCompass,
+} from "@fortawesome/free-regular-svg-icons";
 import "./SearchBar.css";
 
 const SearchBar = (props) => {
+  const { searchFilter } = props;
   return (
     <div className="search-bar-wrapper">
       <div className="social">
@@ -15,6 +20,7 @@ const SearchBar = (props) => {
         <input
           type="text"
           placeholder="Search"
+          onChange={(event) => searchFilter(event.target.value)}
         />
       </form>
       <div className="social-wrapper">
